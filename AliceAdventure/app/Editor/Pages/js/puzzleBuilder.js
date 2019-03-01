dbf9f9a29a6c63d97ce3150838cd7757c7ec8d76
hexiao0225 <hexiao0225@gmail.com>
2019-03-01 15:51:40 -0500

    added object and challenges

--- AliceAdventure/app/Editor/Pages/js/puzzleBuilder.js
+++ AliceAdventure/app/Editor/Pages/js/puzzleBuilder.js
@@ -1,10 +1,9 @@
-// -----------------------goal functions---------------------------------------------------------------
+// -----------------------STEP 1: CHOOSE GOAL goal functions---------------------------------------------------------------
 
 
 function GoToALocation() {
     console.log("go to a location");
-    ChooseNewLocation()
-
+    ChooseNewLocation();
 }
 
 function GetAnObject() {
@@ -20,7 +19,7 @@ function ChangeImageOfAnObject() {
 }
 
 
-// -----------------------location---------------------------------------------------------------
+// -----------------------STEP 2: CHOOSE LOCATION location---------------------------------------------------------------
 
 function ChooseNewLocation() {
     console.log("the new location is");
@@ -29,31 +28,60 @@ function ChooseNewLocation() {
     var goal = document.getElementById("choose-goal");
     goal.innerHTML = "goal: choose new location"
 
-    // var newLocation = '<div>New Location is <span>Scene1</span><span>Scene2</span></div>';
-    // var newLocation = document.createElement('div'); // is a node
-    // newLocation.innerHTML = 'New Location is';
+    var locationOptions = CreateDropDownMenu("Location", ['scene1', 'scene2']);
+    document.getElementById("choose-location").appendChild(locationOptions);
+}
 
-    var locationOptions = CreateDropDownMenu("locations", ['scene1', 'scene2'])
-    // newLocation.appendChild(locationOptions)
-    // document.getElementById("choose-new-location").appendChild(locationOptions);
-    document.getElementById("progressive-builder").appendChild(locationOptions);
+function UpdateLocation(SceneName) {
+    console.log("location is " + SceneName);
+    var location = document.getElementById("choose-location");
+    location.innerHTML += " : " + SceneName;
 
     ChooseHow()
 }
 
 
-// -----------------------how functions---------------------------------------------------------------
+// -----------------------STEP 3: CHOOSE HOW how functions---------------------------------------------------------------
 function ChooseHow() {
-    // var how = '<div>How<span>dropdown</span></div>'
+    var how = CreateDropDownMenu("How", ['By Clicking an Object']);
+    document.getElementById("choose-how").append(how);
 }
+
+function UpdateHow(method) {
+    var how = document.getElementById("choose-how");
+    how.innerHTML += " : " + method;
+
+    if (method === "By Clicking an Object") {
+        ChooseObject();
+    }
+}
+
 
 function ChooseObject() {
     console.log("choose object");
+    //TO-Do: get object list from hierarchy
+    var objectList = CreateDropDownMenu("Object", ['Pineapple', 'Call Button', 'Arrow', 'Radio']);
+    document.getElementById("choose-object").appendChild(objectList);
+
+}
+
+function UpdateObject(object) {
+    var obj = document.getElementById("choose-object");
+    obj.innerHTML += " : " + object;
+
+    AddChallenge();
+
 }
 
 
 
-// -----------------------add complexity functions---------------------------------------------------------------
+// -----------------------STEP 4: MAKE IT MORE CHALLENGING BY XXX functions---------------------------------------------------------------
+
+function AddChallenge() {
+    var challengeList = CreateDropDownMenu("Challenge", ['Add a Lock', 'Add a Guard', 'Add a Switch', 'Looks Good']);
+    document.getElementById("choose-challenge").appendChild(challengeList);
+
+}
 
 function AddALock() {
     console.log("add a lock");
@@ -67,10 +95,34 @@ function AddASwich() {
     console.log("add a switch");
 }
 
+function AddLooksGood() {
+    console.log("looks good");
+}
+
+function UpdateChallenge(challenge) {
+    var c = document.getElementById("choose-challenge");
+    c.innerHTML += " : " + challenge;
+
+    if (challenge === "Add a Lock") {
+        AddALock();
+    } else if (challenge === "Add a Guard") {
+        AddAGuard();
+    } else if (challenge === "Add a Switch") {
+        AddASwich();
+    } else {
+        AddLooksGood();
+    }
+}
+
+
+// -----------------------STEP 5: CHOOSE THE CHALLENGE OBJECTS functions---------------------------------------------------------------
+
+
 
 // -----------------------helper functions---------------------------------------------------------------
 
 
+// return a div of dropdown menu
 function CreateDropDownMenu(name, listOfOptions) {
     //wrapper div
     var wrap = document.createElement("div")
@@ -81,6 +133,7 @@ function CreateDropDownMenu(name, listOfOptions) {
     //first child, btn
     var menu = document.createElement("div");
     menu.className = "dropbtn";
+    // menu.setAttribute("id", "choose-location")
     menu.onmouseover = function (event) {
         dropdownShow(event);
     };
@@ -99,6 +152,29 @@ function CreateDropDownMenu(name, listOfOptions) {
         option.innerHTML = arrayItem.toString();
         // option.setAttribute("class", "dropdown-item")
         option.className = "dropdown-item";
+        if (name === "Goal") {
+            option.onclick = function () {
+                UpdateGoal(arrayItem.toString());
+            }
+        }
+        else if (name === "Location") {
+            option.onclick = function () {
+                UpdateLocation(arrayItem.toString());
+            }
+        } else if (name === "How") {
+            option.onclick = function () {
+                UpdateHow(arrayItem.toString());
+            }
+        } else if (name === "Object") {
+            option.onclick = function () {
+                UpdateObject(arrayItem.toString());
+            }
+        } else if (name === "Challenge") {
+            option.onclick = function () {
+                UpdateChallenge(arrayItem.toString());
+            }
+        }
+
         dropdownContent.appendChild(option);
     });
 
