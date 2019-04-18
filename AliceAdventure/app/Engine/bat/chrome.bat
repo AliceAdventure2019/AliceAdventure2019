1bc60e0f88d5a2a2a33a7849af4cb3637d74c012
Chaojie Zhu <zackspbc@gmail.com>
2019-04-18 13:35:29 -0400

    Fix chrome bat file

--- AliceAdventure/app/Engine/bat/chrome.bat
+++ AliceAdventure/app/Engine/bat/chrome.bat
@@ -1,8 +1,10 @@
-echo on
-::"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
-::"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
-::start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "./index.html"
+REM echo on
+REM ::"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
+REM ::"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
+REM ::start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "./index.html"
 
-start "chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
-start "game" ./index.html
-pause
+REM start "chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
+REM start "game" ./index.html
+REM pause
+
+start "chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files --disable-web-security --user-data-dir=
